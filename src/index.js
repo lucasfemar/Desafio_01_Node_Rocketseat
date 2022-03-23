2db10d3b279399c0755daf5b647b5d319355e058
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2022-03-23 20:46:01 -0300

    Código refatorado

--- src/index.js
+++ src/index.js
@@ -68,24 +68,16 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { id } = request.params;
 
-  const todoAlredyExists = user.todos.find((todo) => todo.id == id);
+  const todo = user.todos.find((todo) => todo.id == id);
 
-  if (!todoAlredyExists) {
-    return response.status(404).json({ error: "Todo not exists." });
+  if(!todo){
+    return response.status(404).json({erro : "Todo not exists"})
   }
 
-  if (user.todos.length > 0) {
-    for (todo of user.todos) {
-      if (todo.id === id) {
-        todo.title = title;
-        todo.deadline = deadline;
-      }
-    }
-  } else {
-    return response.json({ error: "There is no todo to update." });
-  }
+  todo.title = title;
+  todo.deadline = new Date(deadline);
 
-  return response.status(201).send();
+  return response.json(todo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
@@ -93,45 +85,31 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   const { done } = request.body;
   const { id } = request.params;
 
-  const todoAlredyExists = user.todos.find((todo) => todo.id == id);
-
-  if (!todoAlredyExists) {
-    return response.status(404).json({ error: "Todo not exists." });
+  const todo = user.todos.find((todo) => todo.id == id);
+  
+  if (!todo) {    
+    return response.status(404).json({ error: "Todo not exists." });    
   }
 
-  if (user.todos.length > 0) {
-    for (todo of user.todos) {
-      if (todo.id === id) {
-        todo.done = done;
-      }
-    }
-    return response.status(201).send();
-  } else {
-    return response.json({ erro: "There is no todo to update" });
-  }
+  todo.done = done;
+
+  return response.json(todo)
+
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todoAlredyExists = user.todos.find((todo) => todo.id == id);
+  const todoIndex = user.todos.find((todo) => todo.id == id);
 
-  if (!todoAlredyExists) {
-    return response.status(404).json({ error: "Todo not exists." });
+  if(todoIndex === -1){
+    return response.status(404).json({erro : 'Todo not exists'});
   }
 
-  if (user.todos.length > 0) {
-    for (let i = 0; i <= user.todos.length - 1; i++) {
-      if (user.todos[i].id === id) {
-        user.todos.splice(i, 1);
-      }
-    }
-  } else {
-    return response.json({ error: "There is no todo to delete." });
-  }
+  user.todos.splice(todoIndex, 1)
 
-  return response.status(200).send();
+  return response.status(204).send();
 });
 
 module.exports = app;
